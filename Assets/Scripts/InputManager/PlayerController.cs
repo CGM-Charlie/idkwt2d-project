e064dc6f00f2c239ca6907f37d9eb94e659f54a6
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2023-02-13 22:27:38 -0800

    Basic 2D Platformer Controller (#6)
    * Add core libraries
    
    * Movement and jump
    
    * Add Double Jump
    
    * 2D controller, partial implementation coyote
    
    * Add materials
    
    * Fix Coyote jump
    
    * Improve Coyote Jump
    
    * Jump buffer and minor fixes

---
+++ Assets/Scripts/InputManager/PlayerController.cs
@@ -0,0 +1,67 @@
+using System;
+using Kizuna.Player;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// TODO: Investigate if it is possible to have different scripts for controlling the different input maps rather than using a single script
+// TODO: Investigate if is possible to use the Master Singleton Structure for the input maps. Singleton InputManager that regulates all controllers
+
+namespace Kizuna.InputManager {
+    public class PlayerController : MonoBehaviour {
+        // Auto-generated Input Maps
+        public PlayerInputActions playerInputActions;
+        
+        // Input Components
+        [Header("Player Components")] 
+        [SerializeField] private Movement movement;
+
+        private void Awake() {
+            playerInputActions = new PlayerInputActions();
+            SubscribePlayerMap(); // TODO: MOVE TO GAME MANAGER
+        }
+
+        private void Update() {
+            Movement_MoveCharacter();
+        }
+
+        public void SubscribePlayerMap() {
+            // Enable Map
+            playerInputActions.PlayerController.Enable();
+            
+            // Subscribe Methods
+            playerInputActions.PlayerController.Jump.started += Movement_Jump;
+            playerInputActions.PlayerController.Jump.performed += Movement_Jump;
+            playerInputActions.PlayerController.Jump.canceled += Movement_Jump;
+        }
+
+        public void DisablePlayerMap() {
+            // Unsubscribe Methods
+            playerInputActions.PlayerController.Jump.performed -= Movement_Jump;
+            playerInputActions.PlayerController.Jump.canceled -= Movement_Jump;
+            
+            // Disable Map
+            playerInputActions.PlayerController.Disable();
+        }
+
+        // Player Movement
+        private void Movement_MoveCharacter() {
+            var inputVector = playerInputActions.PlayerController.Move.ReadValue<Vector2>();
+            movement.SetInput(inputVector);
+        }
+        
+        private void Movement_Jump(InputAction.CallbackContext context) {
+            if (context.started) {
+                movement.SetJumpBuffer();
+            }
+            
+            if (context.performed) {
+                movement.Jump();
+                movement.SetIsJumping(true);
+            }
+
+            if (context.canceled) {
+                movement.SetIsJumping(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,194 @@
+using System;
+using UnityEngine;
+
+namespace Kizuna.Player {
+    public class Movement : MonoBehaviour {
+        [Header("Components")] 
+        [SerializeField] private Rigidbody2D rb2d;
+        [SerializeField] private Animator animator;
+
+        [Header("Layer Masks")] 
+        [SerializeField] private LayerMask groundLayers;
+
+        // TODO: SET DEFAULT VALUES
+        
+        [Header("Movement Variables")] 
+        [SerializeField] private float movementAcceleration;
+        [SerializeField] private float maxMovementSpeed;
+        [SerializeField] private float groundLinearDrag;
+
+        [Header("Jump Variables")] 
+        [SerializeField] private float jumpForce;
+        [SerializeField] private float airLinealDrag;
+        [SerializeField] private float fallMultiplayer;
+        [SerializeField] private float lowJumpFallMultiplayer;
+        [SerializeField] private int jumps;
+        [SerializeField] private float coyoteJumpTime;
+        [SerializeField] private float jumpBufferTime;
+        private int jumpCounter;
+        private float coyoteJumpTimeCounter;
+        private float jumpBufferCounter; // TODO: FIX JUMP BUFFER
+        private bool isJumping;
+
+        [Header("Ground Collision Variables")] 
+        [SerializeField] private float groundRaycastLength;
+        [SerializeField] private Vector3 groundRaycastOffset;
+        private bool onGround;
+
+        // Private Values
+        private Vector2 inputVector;
+        private float horizontalDirection;
+        
+        // Animator Values
+        private bool isFacingRight = true;
+
+        private bool ChangingDirection => 
+            (rb2d.velocity.x > 0f && horizontalDirection < 0f) || (rb2d.velocity.x < 0f && horizontalDirection > 0f);
+        
+        private void Start() {
+            // Assign Components
+            rb2d = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            
+            inputVector = Vector2.zero;
+            horizontalDirection = inputVector.x;
+        }
+        
+        private void Update() {
+            horizontalDirection = inputVector.x;
+
+            // Animation
+            animator.SetBool("IsGrounded", onGround);
+            animator.SetFloat("HorizontalDirection", Mathf.Abs(horizontalDirection));
+            
+            // Jump Buffer
+            jumpBufferCounter -= Time.deltaTime;
+            
+            // Coyote Jump
+            if (onGround) {
+                coyoteJumpTimeCounter = coyoteJumpTime;
+            } else {
+                coyoteJumpTimeCounter -= Time.deltaTime;
+            }
+
+            if (horizontalDirection < 0f && isFacingRight) {
+                FlipCharacter();
+            } else if (horizontalDirection > 0f && !isFacingRight) {
+                FlipCharacter();
+            }
+
+            if (rb2d.velocity.y < 0f) {
+                animator.SetBool("IsJumping", false);
+                animator.SetBool("IsFalling", true);
+            }
+
+        }
+
+        private void FixedUpdate() {
+            MoveCharacter();
+            IsCharacterOnGround();
+
+            // Rigid Body Drag
+            if (onGround) {
+                jumpCounter = jumps;
+                coyoteJumpTimeCounter = coyoteJumpTime;
+                ApplyGroundLinearDrag();
+                
+                // Animation
+                animator.SetBool("IsJumping", false);
+                animator.SetBool("IsFalling", false);
+            } else {
+                coyoteJumpTimeCounter -= Time.deltaTime;
+                ApplyAirLinearDrag();
+                FallMultiplier();
+            }
+        }
+        
+        private void MoveCharacter() {
+            // Add force
+            rb2d.AddForce(new Vector2(horizontalDirection, 0f) * movementAcceleration);
+            
+            // Clamp Speed
+            if (Mathf.Abs(rb2d.velocity.x) > maxMovementSpeed) {
+                rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxMovementSpeed, rb2d.velocity.y);
+            }
+        }
+
+        // TODO: Fix Drag in vertical axis
+        private void ApplyGroundLinearDrag() {
+            if (Mathf.Abs(horizontalDirection) < 0.4f || ChangingDirection) {
+                rb2d.drag = groundLinearDrag;
+            } else {
+                rb2d.drag = 0;
+            }
+        }
+
+        private void ApplyAirLinearDrag() {
+            rb2d.drag = airLinealDrag;
+        }
+
+        private void IsCharacterOnGround() {
+            var position = transform.position;
+            onGround = 
+                Physics2D.Raycast(position + groundRaycastOffset, Vector2.down, groundRaycastLength, groundLayers) ||                 
+                Physics2D.Raycast(position - groundRaycastOffset, Vector2.down, groundRaycastLength, groundLayers);
+        }
+
+        private void FlipCharacter() {
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0f, 180f, 0f);
+        }
+
+        // Gizmos
+        private void OnDrawGizmos() {
+            var position = transform.position;
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(position + groundRaycastOffset, position + groundRaycastOffset + Vector3.down * groundRaycastLength);
+            Gizmos.DrawLine(position - groundRaycastOffset, position - groundRaycastOffset + Vector3.down * groundRaycastLength);
+
+        }
+        
+        // Fall
+        private void FallMultiplier() {
+            if (rb2d.velocity.y < 0) {
+                rb2d.gravityScale = fallMultiplayer;
+            } else if (rb2d.velocity.y > 0 && !isJumping) {
+                rb2d.gravityScale = lowJumpFallMultiplayer;
+            } else {
+                rb2d.gravityScale = 1f;
+            }
+        }
+
+        // Public Methods
+        
+        public void SetInput(Vector2 vector2) {
+            inputVector = vector2;
+        }
+
+        public void SetIsJumping(bool value) {
+            isJumping = value;
+        }
+
+        public void SetJumpBuffer() {
+            jumpBufferCounter = jumpBufferTime;
+        }
+
+        public void Jump() {
+            if (coyoteJumpTimeCounter < 0f) {
+                jumpCounter--;
+            }
+
+            if ((coyoteJumpTimeCounter > 0f || jumpCounter > 0) && jumpBufferCounter > 0) {
+                coyoteJumpTimeCounter = 0f;
+                jumpBufferCounter = 0f;
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+                rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+            
+            // Animation
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsFalling", false);
+        }
+    }
+}
